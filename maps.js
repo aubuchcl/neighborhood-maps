03aef2e59e926dfd19a14971e0905515380a79d1
Christopher Aubuchon <aubuchcl@gmail.com>
2017-09-05 22:56:03 -0700

    removed if block from populateInfoWindow allowing for markers to be clicked more than once without breaking

--- maps.js
+++ maps.js
@@ -18,34 +18,6 @@ function initMap() {
 	  center: tahoe,
 	  zoom: 13
 	});
-	// These are the real estate listings that will be shown to the user.
-	// Normally we'd have these in a database instead.
-
-	// var largeInfowindow = new google.maps.InfoWindow();
-	// var bounds = new google.maps.LatLngBounds();
-	// // The following group uses the location array to create an array of markers on initialize.
-	// for (var i = 0; i < locations.length; i++) {
-	//   // Get the position from the location array.
-	//   var position = locations[i].location;
-	//   var title = locations[i].title;
-	//   // Create a marker per location, and put into markers array.
-	//   var marker = new google.maps.Marker({
-	//     map: map,
-	//     position: position,
-	//     title: title,
-	//     animation: google.maps.Animation.DROP,
-	//     id: i
-	//   });
-	//   // Push the marker to our array of markers.
-	//   markers.push(marker);
-	//   // Create an onclick event to open an infowindow at each marker.
-	//   marker.addListener('click', function() {
-	//     populateInfoWindow(this, largeInfowindow);
-	//   });
-	//   bounds.extend(markers[i].position);
-	// }
-	// // Extend the boundaries of the map for each marker
-	// map.fitBounds(bounds);
 }
 
 
@@ -69,6 +41,7 @@ function bindMarkers(){
 	  markers.push(marker);
 	  // Create an onclick event to open an infowindow at each marker.
 	  marker.addListener('click', function() {
+	  	console.log(this);
 	    populateInfoWindow(this, largeInfowindow);
 	  });
 	  bounds.extend(markers[i].position);
@@ -81,16 +54,13 @@ function bindMarkers(){
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
 function populateInfoWindow(marker, infowindow) {
-	// Check to make sure the infowindow is not already opened on this marker.
-	if (infowindow.marker != marker) {
-	  infowindow.marker = marker;
-	  infowindow.setContent('<div>' + marker.title + '</div>');
-	  infowindow.open(map, marker);
-	  // Make sure the marker property is cleared if the infowindow is closed.
-	  infowindow.addListener('closeclick',function(){
-	    infowindow.setMarker = null;
-	  });
-	}
+  infowindow.marker = marker;
+  infowindow.setContent('<div>' + marker.title + '</div>');
+  infowindow.open(map, marker);
+  // Make sure the marker property is cleared if the infowindow is closed.
+  infowindow.addListener('closeclick',function(){
+    infowindow.setMarker = null;
+  });
 }
 
 //when the doc is ready make the first marker bind.
