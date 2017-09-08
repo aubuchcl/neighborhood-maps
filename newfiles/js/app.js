57ebd66414334d23e6adf3dd517588dcd47265cc
Christopher Aubuchon <aubuchcl@gmail.com>
2017-09-08 00:25:19 -0700

    list control through click binding working in playground

--- newfiles/js/app.js
+++ newfiles/js/app.js
@@ -1,4 +1,21 @@
+// model
+var locations = [
+	  {title: 'Northstar', location: {lat: 39.27324, lng: -120.1624164}},
+	  {title: 'Squaw Valley', location: {lat: 39.2115732, lng: -120.1985282}},
+	  {title: 'Sugar Bowl', location: {lat: 39.3043494, lng: -120.3357567}},
+	  {title: 'Tahoe Donner Ski Area', location: {lat: 39.3524057, lng: -120.271583}},
+	  {title: 'Alpine Meadows', location: {lat: 39.1574066, lng: -120.2390835}}
+	];
 
+//Marker constructor
+
+function Location(title, location){
+	var self = this;
+
+	self.title = title;
+	self.location = location;
+
+}
 
 
 
@@ -6,20 +23,52 @@
 // create the apps viewmodel
 
 function AppViewModel(){
-	this.searchBar = ko.observable('');
-	this.title = ko.observable("NorthStar");
-	this.location = ko.observable("{lat: 100, long: 200}");
+	var self = this;
 
-	this.searchResults = ko.computed(function(){
-		return this.searchBar();
-	}, this)
+	self.searchBar = ko.observable('');
+	// title and location now come from the Location objects
+	// self.title = ko.observable("NorthStar");
+	// self.location = ko.observable("{lat: 100, long: 200}");
 
-	this.capitalizeInput = function(){
-		var currentVal = this.searchBar();
-		this.searchBar(currentVal.toUpperCase());
+	self.searchResults = ko.computed(function(){
+		return self.searchBar();
+	}, self)
+
+	self.capitalizeInput = function(){
+		var currentVal = self.searchBar();
+		self.searchBar(currentVal.toUpperCase());
 
 	};
+
+	//locations initialized
+	self.locations = ko.observableArray();
+
+	locations.forEach(function(location){
+		self.locations.push(new Location(location.title, location.location));
+	});
+
+
+	ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+	self.filterList = function(){
+		console.log(self.locations().length)
+		for(let i=0; i<self.locations().length; i++){
+			if(self.locations()[i].title.toUpperCase().indexOf(self.searchBar()) > -1){
+				console.log(li[i])
+				li[i].style.display = "";
+			}
+		}
+
+
+	}
+	$('#testButton').click(function(){
+		self.filterList();
+	})
+
+
 }
+
 
 // Activate knockout.js
 ko.applyBindings(new AppViewModel());
